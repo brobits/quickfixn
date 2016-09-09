5f8e4e98663831efc588cd47ab60d8e2c1e6564b
brobits <matt@brobits.com>
2016-09-09 14:27:00 -0500

    updated variable

--- generator/Specification.ps1
+++ generator/Specification.ps1
@@ -6,7 +6,7 @@ function Generate-Message-Class()
   param($dd)
   [string]$fixVersion = Fix-Version $dd
   [string]$template = Load-Template generator/templates/Message.cst 0
-  [string]$code = $template -replace "<%version%>", $fixVersion
+  [string]$code = $template -replace "<#version#>", $fixVersion
   [string]$path = [string]::Format("QuickFIXn/Message/{0}/Message.cs", $fixVersion)
   Write-Code $path $code
 }
